84fbe71deafb547c9afbb1d230c8fe5cf1f2b526
Miguel Angel Garcia <miguelangel.garcia@gmail.com>
2014-12-25 18:55:26 +0100

    Walking over directories and improving documentation

--- README.rst
+++ README.rst
@@ -6,11 +6,104 @@ VERSION         DOWNLOADS        TESTS      COVERAGE
 
 **STILL UNDER HEAVY DEVELOPMENT**
 
+Checkout the repository_.
 
 Goal and Philosophy
 ===================
 
 Manages templates to allow projects inception.
+
+I'm tired of doing repetitive steps whenever I create a new project: configure Travis, add the ``.gitignore``, create a new ``setup.py``, etc. So I decided to automate it.
+
+This project is in its first stages, so do not be too tidy.
+
+
+How to use it
+=============
+
+Installation
+------------
+
+From pypi:
+
+.. code::
+
+   pip install inception
+
+From the main repository:
+
+.. code::
+
+   git clone https://github.com/magmax/inception.git
+
+
+Usage
+-----
+
+As easy as:
+
+.. code::
+
+   python inception/__main__.py --template-path TEMPLATE -o OUTPUT_PATH
+
+
+Template creation
+=================
+
+The idea is to maintain easy templates. So, there are two main stages:
+
+#. Ask the user for some data
+#. Generate the result.
+
+Let's see how to configure these steps.
+
+settings.py
+-----------
+
+This file should contain an uppercase variable called ``QUESTIONS``, containing a list of questions. It uses the inquirer_ format, and you can return a string.
+
+Example:
+
+.. code::
+
+    QUESTIONS = [
+        { "kind": "text",
+          "name": "name",
+          "message": "What's your name"
+        },
+        { "kind": "text",
+          "name": "surname",
+          "message": "What's your surname"
+        },
+        { "kind": "list",
+          "name": "size",
+          "message": "What size do you need?",
+          "choices": ["Jumbo", "Large", "Standard", "Medium", "Small", "Micro"]
+        }
+    ]
+
+
+files
+-----
+
+In order to generate the result, you have to store files in the ``files`` directory.
+
+Any directory in the ``files`` directory will be created in the ``output`` directory.
+Any file ending in ``.jinja`` will be managed as a jinja_ template, using the variables retrieved by the ``QUESTIONS``.
+Any other file will be copied as it is.
+
+Existing files won't be overriden. You have to remove them in order to regenerate them.
+
+To do list
+==========
+
+Things I'd like to add to inception:
+
+- a Downloader class, that retrieves the package from github.
+- a file with the list of available templates, to use its name instead its directory. This will allow to improve the Downloader class.
+- better output
+- tests.
+- debianize it.
 
 
 License
@@ -44,3 +137,6 @@ Licensed under `the MIT license`_.
 
 .. _the MIT license: http://opensource.org/licenses/MIT
 .. _download the lastest zip: https://pypi.python.org/pypi/inception
+.. _inquirer: https://travis-ci.org/magmax/python-inquirer
+.. _repository: https://travis-ci.org/magmax/inception
+.. _jinja: http://jinja.pocoo.org/
--- inception/main.py
+++ inception/main.py
@@ -1,12 +1,30 @@
 #!/usr/bin/env python
 
+# Copyright (C) 2014 Miguel Angel Garcia <miguelangel.garcia@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import os
 import argparse
 import jinja2
+import logging
 
 import inquirer
 
 from version import APP
+
+LOGGER = logging.getLogger(__name__)
 
 
 class Loader(object):
@@ -22,6 +40,10 @@ class Loader(object):
             exec(fd.read(), {}, config)
         return config
 
+    @property
+    def file_path(self):
+        return os.path.join(self._path, 'files')
+
 
 class Runner(object):
     def __init__(self, loader):
@@ -29,10 +51,10 @@ class Runner(object):
         self._config = None
         self._variables = None
 
-    def run(self):
+    def run(self, output):
         self.load_config()
         self.inquire()
-        self.apply()
+        self.generate(output)
 
     def load_config(self):
         self._config = self._loader.config
@@ -48,26 +70,73 @@ class Runner(object):
             q = inquirer.questions.load_from_list(questions)
             self._variables = inquirer.prompt(q)
 
-    def apply(self):
-        pass
-        # To be done: walk over self._config._path and:
-        #   - create every directory mirror
-        #   - apply the template and save it in the mirror.
+    def generate(self, output):
+        basepathlen = len(self._loader.file_path) + 1
+        for root, dirs, files in os.walk(self._loader.file_path):
+            for d in dirs:
+                origin = os.path.join(root, d)
+                path = os.path.join(output, origin[basepathlen:])
+                if not os.path.exists(path):
+                    LOGGER.info('Creating directory %s', path)
+                    os.makedirs(path)
+            for f in files:
+                origin = os.path.join(root, f)
+                path = os.path.join(output, origin[basepathlen:])
+                with open(origin) as fd:
+                    if path.endswith('.jinja'):
+                        target = path[:-len('.jinja')]
+                        LOGGER.info('Applying template %s to %s',
+                                    origin, target)
+                        template = jinja2.Template(fd.read())
+                        content = template.render(self._variables)
+                    else:
+                        target = path
+                        LOGGER.info('Copying file %s to %s', origin, target)
+                        content = fd.read()
+                if os.path.exists(target):
+                    LOGGER.info(
+                        'File "%s" already exists and will not be overriden.',
+                        target)
+                    continue
+                with open(target, 'w+') as fd:
+                    fd.write(content)
 
-        #template = jinja2.Template('Hello {{ name }}!')
-        #template.render(name='John Doe')
+
+def logging_setup(verbose):
+    if verbose:
+        FORMAT = '%(asctime)s [%(levelname)s] %(message)s'
+        DATEFORMAT = ''
+        LEVEL = logging.DEBUG
+    else:
+        FORMAT = '%()s %(message)s'
+        DATEFORMAT = ''
+        LEVEL = logging.INFO
+    formatter = logging.Formatter(FORMAT, DATEFORMAT)
+    handler = logging.StreamHandler()
+    handler.setFormatter(formatter)
+    LOGGER.addHandler(handler)
+    LOGGER.setLevel(LEVEL)
+    LOGGER.debug('Verbose mode activated')
 
 
 def main():
     parser = argparse.ArgumentParser(description=APP.description)
-    parser.add_argument('--path', required=True,
+    parser.add_argument('--template-path', dest="path", required=True,
                         help='Path to template to be applied.')
+
+    parser.add_argument('-o', '--output',
+                        help='Where the output should be put.')
+
+    parser.add_argument('--verbose', action="store_true", default=False,
+                        help='Verbose mode.')
 
     args = parser.parse_args()
 
+    logging_setup(args.verbose)
+
     loader = Loader(args.path)
     runner = Runner(loader)
-    runner.run()
+    runner.run(args.output)
 
 
 if __name__ == '__main__':
